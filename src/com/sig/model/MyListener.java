e0da648c2f807c5234c969700116584b03eae0c4
alaaa <eng.lala21@gmail.com>
2022-04-15 21:45:31 +0200

    new class

---
+++ src/com/sig/model/MyListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sig.model;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author pc
+ */
+public class MyListener implements ActionListener {
+    
+  
+    
+}
