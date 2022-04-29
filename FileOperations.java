99d6c86de713e3393a086a6209869959a2b45564
alaaa21 <89028370+alaaa21@users.noreply.github.com>
2022-04-29 20:38:31 +0200

    Add files via upload

---
+++ FileOperations.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sig.model;
+import java.util.*;
+import java.io.*;
+/**
+ *
+ * @author pc
+ */
+public class FileOperations {
+  public static void main(String args[]){
+      File f= new File("E:\\Invoice_Project\\SalesInvoice\\InvoiceHeader.csv");
+  }
+       
+    
+}
